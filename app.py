b420f1a3f573f85339d26009d0fe7a0716639626
Daniel B <daniel.m.bassett@outlook.com>
2023-12-08 12:16:19 -0600

    added tabs, removed pagination, added company count

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 import io
 
 import pandas as pd
+import numpy as np
 import plotly_express as px
 
 from openai import OpenAI
@@ -44,7 +45,8 @@ def split_frame(input_df, rows):
     return df
 
 
-st.markdown('## InsightGrant', unsafe_allow_html=True)
+def top_10(df):
+  return df.head(10)
 
 
 nih_keywords_summary = load_data('data/nih_keywords_summary.parquet')
@@ -54,80 +56,138 @@ nih_keywords_summary = nih_keywords_summary.drop(columns=['agency_code', 'agency
 nih_keywords_summary['abstract_text'] = nih_keywords_summary['abstract_text'].str.replace('’', "'")
 nih_keywords_summary['keywords'] = nih_keywords_summary['keywords'].apply(lambda lst: [word.replace('’', "'") for word in lst])
 nih_keywords_summary['keywords'] = nih_keywords_summary['keywords'].apply(lambda lst: [word.lower() for word in lst])
-nih_keywords_summary = nih_keywords_summary.rename(columns={'PI_count': 'Principal Investigators Count', 'abstract_text': 'Abstract', 'award_amount': 'Award Amount',
-                                                            'appl_id': 'Application ID', 'fiscal_year': 'Fiscal Year', 'keywords': 'Keywords', 'n_employees': 'Employee Count',
+nih_keywords_summary = nih_keywords_summary.rename(columns={'agency': 'Agency', 'PI_count': 'Principal Investigators Count', 'abstract_text': 'Abstract', 'award_amount': 'Award Amount',
+                                                            'appl_id': 'Application ID', 'fiscal_year': 'Fiscal_Year', 'keywords': 'Keywords', 'n_employees': 'Employee Count',
                                                             'opportunity_number': 'Opportunity Number', 'org_name': 'Company', 'phr_text': 'Public Health Relevance',
                                                             'project_detail_url': 'Project URL', 'project_end_date': 'End Date', 'project_start_date': 'Start Date',
                                                             'project_title': 'Project Title', 'publications_count': 'Publications Count', 
                                                             'spending_categories_desc': 'Spending Categories', 'terms': 'Terms', 'summary': 'Summary', 'cfda_code': 'CFDA Code',
-                                                            'cong_dist': 'Congressional Dist.', 'direct_cost_amt': 'Direct Costs', 'indirect_cost_amt': 'Indirect Costs'
+                                                            'cong_dist': 'Congressional Dist.', 'direct_cost_amt': 'Direct Costs', 'indirect_cost_amt': 'Indirect Costs', 'city': 'City'
                                                             })
 
-
-keywords_column = st.columns((6, 6))
-with keywords_column[0]:
-    keywords = st.text_input('Enter keywords')
-    keywords = keywords.split(' ')
-
-columns_column = st.columns((6,6))
-with columns_column[0]:
-    with st.expander('Add/Remove Columns'):
-        columns = st.multiselect('Select Columns', options=nih_keywords_summary.columns.sort_values(), default=['Company', 'Summary', 'Keywords'])
-
-
-# FILTER KEYWORDS
-temp_df = nih_keywords_summary.copy()
-for keyword in keywords:
-    keyword_filter = temp_df['Abstract'].str.contains(keyword, case=False)
-    temp_df = temp_df[keyword_filter]
-temp_df = temp_df[columns]
+# Define Tabs
+# eda_tab, agency_tab, keyword_tab, changelog_tab = st.tabs(['EDA', 'Agency', 'Keyword', 'Changelog'])
+keyword_tab, changelog_tab = st.tabs(['Keyword', 'Changelog'])
 
 
 
-main_columns = st.columns((6, 6))
-
-# DATAFRAME COLUMN
-with main_columns[0]:
-    pagination = st.container()
-
-    bottom_menu = st.columns((3, 5, 4))
-    with bottom_menu[2]:
-        batch_size = st.selectbox("Page Size", options=[25, 50, 100])
-    with bottom_menu[1]:
-        total_pages = (
-            int(len(temp_df) / batch_size) if int(len(temp_df) / batch_size) > 0 else 1
-        )
-        current_page = st.number_input(
-            "Page", min_value=1, max_value=total_pages, step=1
-        )
-    with bottom_menu[0]:
-        st.markdown(f"Page **{current_page}** of **{total_pages}** ")
-
-    pages = split_frame(temp_df, batch_size)
-    pagination.data_editor(data=pages[current_page - 1], use_container_width=True, hide_index=True, column_config={"Select": st.column_config.CheckboxColumn(required=True)})
+# # EXPLORATORY DATA ANALYSIS
+# with eda_tab:
+#     st.write('Agency Tab')
+    
+#     agency_value_counts = (nih_keywords_summary
+#                            .query('Fiscal_Year != 2024')
+#                            .groupby(['Fiscal_Year', 'Agency'], as_index=False)
+#                            .size()
+#                            .sort_values(by=['Fiscal_Year', 'size'], ascending=[True, False])
+#                            .groupby('Fiscal_Year')
+#                            .apply(top_10)
+#                            .reset_index(drop=True)
+#                            )
+#     fig = px.bar(agency_value_counts.sort_values(by=['Fiscal_Year', 'size']), x="size", y="Agency", facet_col="Fiscal_Year", orientation='h')
+#     st.plotly_chart(fig, use_container_width=True)
 
 
-with main_columns[1]:
-# Keyword Rankings
-    if 'Keywords' in temp_df.columns:
-
-        # st.markdown('## Word Frequency', unsafe_allow_html=True)
-
-        keywords = [word for words in temp_df.Keywords for word in words]
-        keywords = pd.Series(keywords)
-        st.write((keywords.value_counts() / len(temp_df) * 100).round(1).rename('percentage of abstracts'))
+# # AGENCY 
+# with agency_tab:
+#     st.write(nih_keywords_summary)  
 
 
-st.markdown('#### Changelog', unsafe_allow_html=True)
-st.markdown("""
-        ##### 12/7/2023:
-            - Added 2018-2022 data (~10,000 rows)
-            - More columns added (award amount, direct/indirect cost, agency)
-            - Cleaned up the keywords column
-""", unsafe_allow_html=True)
+# KEYWORD SEARCH
+with keyword_tab:
+    st.markdown('## InsightGrant', unsafe_allow_html=True)
+
+    # WIDGETS
+    keywords_column = st.columns((6, 6))
+    with keywords_column[0]:
+        keywords = st.text_input('Enter keywords')
+        keywords = keywords.split(' ')
+
+    columns_column = st.columns((6,6))
+    with columns_column[0]:
+        with st.expander('Add/Remove Columns'):
+            columns = st.multiselect('Select Columns', options=nih_keywords_summary.columns.sort_values(), default=['Company', 'Summary', 'Keywords'])
+    with columns_column[1]:
+        with st.expander('Year(s)'):
+            year_options = list(nih_keywords_summary.Fiscal_Year.unique())
+            years = st.multiselect('Select Year(s)', year_options, default=2023)
+
+    # MAKE NIH COPY
+    temp_df = nih_keywords_summary.copy()
+
+    # FILTER YEARS
+    if years:
+        years_filter = temp_df['Fiscal_Year'].isin(years)
+        temp_df = temp_df[years_filter]
+
+    # FILTER KEYWORDS
+    for keyword in keywords:
+        keyword_filter = temp_df['Abstract'].str.contains(keyword, case=False)
+        temp_df = temp_df[keyword_filter]
+
+    # FILTER COLUMNS
+    temp_df = temp_df[columns]
+
+    main_columns = st.columns((6, 6))
+
+    # DATAFRAME COLUMN
+    with main_columns[0]:
+        pagination = st.container()
+
+        # bottom_menu = st.columns((3, 5, 4))
+        # with bottom_menu[2]:
+        #     batch_size = st.selectbox("Page Size", options=[25, 50, 100])
+        # with bottom_menu[1]:
+        #     total_pages = (
+        #         int(len(temp_df) / batch_size) if int(len(temp_df) / batch_size) > 0 else 1
+        #     )
+        #     current_page = st.number_input(
+        #         "Page", min_value=1, max_value=total_pages, step=1
+        #     )
+        # with bottom_menu[0]:
+        #     st.markdown(f"Page **{current_page}** of **{total_pages}** ")
+
+        # pages = split_frame(temp_df, batch_size)
+        # pagination.data_editor(data=pages[current_page - 1], use_container_width=True, hide_index=True, column_config={"Select": st.column_config.CheckboxColumn(required=True)})
+        pagination.data_editor(data=temp_df, use_container_width=True, hide_index=True)
+        st.write(f'Companies: {len(temp_df.Company.unique())}')
+
+        # AGGREGATIONS AT BOTTOM OF PAGE
+        # agg_df = (nih_keywords_summary.loc[temp_df.index]
+        #           .groupby(['Agency']).size()
+        #           )
+        # st.write(agg_df)
+        # st.write(nih_keywords_summary.loc[temp_df.index][['Company', 'Fiscal_Year']].drop_duplicates().Fiscal_Year.value_counts().rename('Companies').sort_index(ascending=False))
+
+    with main_columns[1]:
+    # Keyword Rankings
+        if 'Keywords' in temp_df.columns:
+
+            # st.markdown('## Word Frequency', unsafe_allow_html=True)
+
+            keywords = [word for words in temp_df.Keywords for word in words]
+            keywords = pd.Series(keywords)
+            st.write((keywords.value_counts() / len(temp_df) * 100).round(1).rename('percentage of abstracts'))
 
 
-# st.write('TEST')
+with changelog_tab:
+    st.markdown('#### Changelog', unsafe_allow_html=True)
+    st.markdown("""
+            ##### 12/7/2023:
+                - Added 2018-2022 data (~10,000 rows)
+                - More columns added (award amount, direct/indirect cost, agency)
+                - Cleaned up the keywords column
+    """, unsafe_allow_html=True)
+    st.markdown("""
+            ##### 12/8/2023:
+                - Added filter for Years (default is 2023)
+                - Added Count for Companies that fit keyword criteria
+                - Added Tabs
+                - Removed pagination from data frame (for now)
+    """, unsafe_allow_html=True)
+
+
+# st.write('GRID OPTION BUILDER')
 
 # def dataframe_with_selections(df):
 #     df_with_selections = df.copy()
