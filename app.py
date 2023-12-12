304c88ab195c1edb8d8e2b069df7bb65e1f6dfa1
Daniel B <daniel.m.bassett@outlook.com>
2023-12-12 08:29:59 -0600

    added selenium scraper

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from streamlit_option_menu import option_menu
 
 import time
 import io
+import json
 
 import pandas as pd
 import numpy as np
@@ -12,8 +13,24 @@ import plotly_express as px
 
 from openai import OpenAI
 
+from urllib.parse import urlparse, urlunparse
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# initialize session states
+if 'keywords' not in st.session_state:
+    st.session_state['keywords'] = False
+    st.session_state['summary'] = False
+
+
 st.set_page_config(layout='wide')
 
+# load openai client
 try:
     with open('../../openai_key.txt', 'r') as file:
         API_KEY = file.read().strip()
@@ -33,6 +50,44 @@ def load_data(path):
     if '.csv' in path:
         return pd.read_csv(path)
     
+
+def sort_key(item):
+    return not ('about' in item or 'story' in item or 'mission' in item or 'who-we' in item or 'vision' in item)
+
+
+def url_parse(internal_hrefs):
+    normalized_urls = set()
+    for url in internal_hrefs:
+        parsed_url  = urlparse(url)
+        clean_url = urlunparse(parsed_url._replace(fragment=''))
+        normalized_urls.add(clean_url)
+    normalized_urls = list(normalized_urls)
+    return normalized_urls
+
+
+# @st.cache_resource
+def get_driver():
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    options.add_experimental_option("detach", True)
+    return webdriver.Chrome(options=options)
+
+
+def get_summary(page_text):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        response_format={ "type": "json_object" },
+        messages=[
+            {"role": "system", 
+             "content": "This is the page text scraped off a startup website. Pull out the keywords and a 3-4 sentence summary of what this company does. Format it in json:"},
+            {"role": "user", 
+             "content": f'Extract from the following page text: [{page_text}]'}
+        ]
+        )
+    return json.loads(completion.choices[0].message.content)
+
 
 # @st.cache_data(show_spinner=False)
 # def split_frame(input_df, rows):
@@ -95,7 +150,7 @@ nih_keywords_summary = nih_keywords_summary.rename(columns={'agency': 'Agency', 
 
 st.markdown('## InsightGrant', unsafe_allow_html=True)
 
-keyword_tab, changelog_tab = st.tabs(['Keyword', 'Changelog'])
+keyword_tab, webscrape, changelog_tab = st.tabs(['Keyword', 'WebScrape', 'Changelog'])
 
 # KEYWORD SEARCH
 with keyword_tab:
@@ -209,7 +264,101 @@ with keyword_tab:
             keywords = pd.Series(keywords)
             st.write((keywords.value_counts() / len(temp_df) * 100).round(1).rename('percentage of abstracts'))
 
+# WEBSCRAPE
+with webscrape:
+    # get user url input
+    url = st.text_input('Enter url')
 
+    # prepend http if not found in url
+    if 'http' not in url:
+        url = 'https://' + url
+
+    st.session_state['message'] = st.empty()
+
+    # button for scraping site
+    if st.button('Scrape Site'):
+
+        st.session_state['message'].text(f'Initializing Scraper...')
+
+        driver = get_driver()
+        driver.get(url)
+
+        st.session_state['message'].text(f"Loading {url}...")
+
+        # parse url
+        time.sleep(2)
+        url = driver.current_url
+        # st.write(url)
+        parsed_url = urlparse(url)
+        hostname = parsed_url.hostname
+        # st.write(hostname)
+
+        # Use BeautifulSoup to parse and scrape
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        page_text = soup.get_text()
+
+        # get anchor tags
+        anchor_tags = driver.find_elements(By.TAG_NAME, "a")
+
+        # Extract href attributes
+        hrefs = [tag.get_attribute('href') for tag in anchor_tags if tag.get_attribute('href')]
+        
+        # drop duplicate hrefs
+        hrefs = set(hrefs)
+
+        # create list of internal hrefs
+        internal_list = [f'{url}', hostname.split('.')[0]]
+        internal_hrefs = [href for href in hrefs if all(include in href for include in internal_list)]
+        internal_hrefs = url_parse(internal_hrefs)
+        internal_hrefs = set(internal_hrefs)
+        internal_hrefs = list(internal_hrefs)
+        internal_hrefs = [href for href in internal_hrefs if 'about' in href or 'story' in href or 'mission' in href or 'who-we' in href or 'vision' in href]
+        # internal_hrefs = sorted(internal_hrefs, key=sort_key)
+
+
+        for url in internal_hrefs:
+            st.session_state['message'].text(f"Loading {url}...")
+            driver.get(url)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            page_text += soup.get_text()
+
+        # Close the browser
+        driver.quit()
+
+        # Display the scraped text
+        # st.write(internal_hrefs)
+        # st.write(page_text)
+
+        st.session_state['message'].text(f"Extracting Keywords/Summary...")
+        data = get_summary(page_text)
+        # st.write(data)
+
+        # st.markdown('### Summary:', unsafe_allow_html=True)
+        # st.write(data['summary'])
+
+        # st.markdown('### Keywords:', unsafe_allow_html=True)
+        # st.write(data['keywords'])
+
+        # st.session_state['message'].empty()
+        st.session_state['keywords'] = data['keywords']
+        st.session_state['summary'] = data['summary']
+    
+    if st.session_state['keywords']:
+        webscrape_columns = st.columns((4,4,4))
+        with webscrape_columns[0]:
+            st.markdown('### Summary:', unsafe_allow_html=True)
+            st.write(st.session_state['summary'])
+        with webscrape_columns[1]:
+            st.markdown('### Keywords:', unsafe_allow_html=True)
+            st.write(st.session_state['keywords'])
+    
+        st.session_state['message'].empty()
+
+
+
+# CHANGE LOG
 with changelog_tab:
     st.markdown('#### Changelog', unsafe_allow_html=True)
     st.markdown("""
