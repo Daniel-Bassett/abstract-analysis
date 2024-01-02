2674d3e04d0a6973928661bbb978cc06cf660e88
Daniel B <daniel.m.bassett@outlook.com>
2024-01-02 08:02:06 -0600

    security

--- app.py
+++ app.py
@@ -71,6 +71,9 @@ def get_driver():
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--ignore-certificate-errors')  # Ignore certificate errors
+    options.add_argument('--allow-running-insecure-content')  # Allow running insecure content
+    options.add_argument('--disable-web-security')  # Bypass CSP; use with caution
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     options.add_experimental_option("detach", True)
     return webdriver.Chrome(options=options)
