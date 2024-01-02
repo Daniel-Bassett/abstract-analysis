59825a1ebafdfd27c6c12c149bcc4ca82887988d
Daniel B <daniel.m.bassett@outlook.com>
2024-01-02 08:14:21 -0600

    dev shm usage

--- app.py
+++ app.py
@@ -71,6 +71,7 @@ def get_driver():
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--ignore-certificate-errors')  # Ignore certificate errors
     options.add_argument('--allow-running-insecure-content')  # Allow running insecure content
     options.add_argument('--disable-web-security')  # Bypass CSP; use with caution
